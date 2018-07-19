a7c72648f515e8512d0f47d464478869e23065fc
Twixus Vulgaris <twixenator@gmail.com>
2018-07-19 03:39:13 +0300

    Partly debugged

--- LinksTableGenerator.php
+++ LinksTableGenerator.php
@@ -1,7 +1,7 @@
-#/usr/bin/php
+#!/usr/bin/php
 <?php
 //Настройки
-$dtFile = $adv[1];
+$dtFile = 'probe_dt_export1.txt';
 //$bookmarksFile = $adv[2];
 
 $allPortals = [];
@@ -19,45 +19,55 @@ foreach($drawing as $line) {
 	if (($line->type == 'polyline')){
 		$prev = [];
         foreach($line->latLngs as $latLng) {
-            if ($minX == -1 || $minX > $latLng->lng) {$minX = $latLng->lat;}
-            if ($maxX == -1 || $maxX < $latLng->lng) {$maxX = $latLng->lat;}
-            if ($minY == -1 || $minY > $latLng->lat) {$minY = $latLng->lng;}
-            if ($maxY == -1 || $maxY < $latLng->lat) {$maxY = $latLng->lng;}
-            $portal = ['lat'=>$latLng->lat, 'lng'=>$latLng->lng];
+            if ($minX == -1 || $minX > $latLng->lng) {$minX = $latLng->lng;}
+            if ($maxX == -1 || $maxX < $latLng->lng) {$maxX = $latLng->lng;}
+            if ($minY == -1 || $minY > $latLng->lat) {$minY = $latLng->lat;}
+            if ($maxY == -1 || $maxY < $latLng->lat) {$maxY = $latLng->lat;}
+            $portal = ['lat'=>$latLng->lat, 'lng'=>$latLng->lng, 'title'=>$latLng->title];
             if (!in_array($portal, $allPortals)) {$allPortals[] = $portal;}
             if ($prev != []) {$allLinks[] = [$prev, $portal];}
             $prev = $portal;
         }
 	}
 }
+
 //Находим внешние углы нашей матрёшки и параллельно считаем общее количество линков каждого портала
-foreach ($allPortals as $portal) {
+foreach ($allPortals as &$portal) {
 	$lnkCount = 0;
 	foreach ($allLinks as $link) {
 		if ($link[0] == $portal || $link[1] == $portal) {$lnkCount++;}
 	}
 	$portal['linkCount'] = $lnkCount;
-	if ($portal['lng'] == minX || $portal['lng'] == maxX || $portal['lat'] == minY || $portal['lat'] == maxY) {
+	if ($portal['lng'] == $minX || $portal['lng'] == $maxX || $portal['lat'] == $minY || $portal['lat'] == $maxY) {
+	    //var_dump($portal);
 		$outCorners[] = $portal;
 		$portal['lat'] = 0;
-		$portal['lng'] = 0; 
+		$portal['lng'] = 0;
+		var_dump($portal);
 	}
 }
+var_dump($outCorners);
+//var_dump($allPortals);
 //Собираем порталы в грядки
 $ridges = [];
 foreach ($outCorners as $portal) {
 	$ridge = [$portal];
 	$findNext = true;
 	while ($findNext) {
+		echo "while\n";
 	    $closestIdx = FindClosest($portal, $allPortals);
+	    var_dump($closestIdx);
 	    if ($allPortals[$closestIdx]['lat'] != 0 && $allPortals[$closestIdx]['lng'] != 0) {
+	    	echo "Detecting\n";
 	        if (!DetectLink($portal, $allPortals[$closestIdx], $allLinks)) {
 		        $portal = $allPortals[$closestIdx];
 		        array_unshift($ridge, $portal);
 		        $allPortals[$closestIdx]['lat'] = 0;
 		        $allPortals[$closestIdx]['lng'] = 0;
-	        } else {$findNext = false;}
-        }
+	        } else {$findNext = false;} 
+	        var_dump($findNext);
+	        var_dump($ridge);
+        } 
     }
     $ridges[] = $ridge;
 }
@@ -86,11 +96,15 @@ function FindClosest($portal1, $allPortals)
 function DetectLink($portal1, $portal2, $allLinks) {
 	$linked = false;
     foreach ($allLinks as $link) {
-    	if (($link[0] == $portal1 && $link[1] == $portal2) || ($link[0] == $portal2 && $link[1] == $portal1)) { 
+    	//var_dump($link);
+    	//var_dump($portal1);
+    	//var_dump($portal2);
+    	if (($link[0]['lat'] == $portal1['lat'] && $link[0]['lng'] == $portal1['lng'] && $link[1]['lat'] == $portal2['lat'] && $link[1]['lng'] == $portal2['lng']) || ($link[1]['lat'] == $portal1['lat'] && $link[1]['lng'] == $portal1['lng'] && $link[0]['lat'] == $portal2['lat'] && $link[0]['lng'] == $portal2['lng'])) { 
             $linked = true;
     	}
     	if ($linked) {break;}
     }
+    var_dump($linked);
     return $linked;
 }
 ?>
